c6e6fec530bb5c42b4bf7835ab84646ee6a4d43d
Jordi Casesnoves <jordicasesnoves@gmail.com>
2024-11-09 19:01:22 +0100

    Added experience and about me sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["EB Garamond", "serif"],
+        sans: ["Outfit", "sans-serif"]
+      },
+      colors: {
+        body: "#37503C",
+        primary: "#5B8363",
+        secondary: "#bcd1c0",
+        background: "#F1EBE1"
+      }
+    }
+  },
+  plugins: []
+};
